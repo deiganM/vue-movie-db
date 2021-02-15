475f536d354d504e96fbdd418ac9b8748f8e24fd
Deigan Millikin <dmillikin@fieldsafesolutions.com>
2021-02-15 11:01:23 -0700

    Post- Creating Our Movies Listing, images will not be displaying

---
+++ src/components/Movie.vue
@@ -0,0 +1,13 @@
+<template>
+  <img :src="movie.posterPath" :alt="movie.title" />
+</template>
+
+<script>
+export default {
+  // This may break the app according to a comment?
+  name: 'Movie',
+  props: ['movie'],
+}
+</script>
+
+<style></style>
--- src/components/MoviesList.vue
+++ src/components/MoviesList.vue
@@ -1,15 +1,18 @@
 <template>
-  <div>
-    {{ movie.title }}
-  </div>
+  <ul>
+    <li v-for="(movie, index) in movies" :key="index">
+      <Movie :movie="movie" />
+    </li>
+  </ul>
 </template>
 
 <script>
+import Movie from './Movie.vue'
 export default {
   name: 'MoviesList',
   data() {
     return {
-      movie: {},
+      movies: [],
     }
   },
   created: function() {
@@ -19,14 +22,17 @@ export default {
     async fetchData() {
       try {
         const res = await fetch(
-          'https://api.themoviedb.org/3/movie/550?api_key=fa44637c4e3d45f7962fe730ff605501'
+          'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=fa44637c4e3d45f7962fe730ff605501'
         )
-        const movie = await res.json()
-        this.movie = movie
+        const movies = await res.json()
+        this.movies = movies.results
       } catch (e) {
         console.log(e)
       }
     },
+  },
+  components: {
+    Movie,
   },
 }
 </script>
