6318572ff07519f893979620fe28a89e17ecb608
Deigan Millikin <dmillikin@fieldsafesolutions.com>
2021-02-15 10:22:57 -0700

    Hitting The MovieDB API with Methods

---
+++ src/components/MoviesList.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    {{ movie.title }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MoviesList',
+  data() {
+    return {
+      movie: {},
+    }
+  },
+  created: function() {
+    this.fetchData()
+  },
+  methods: {
+    async fetchData() {
+      try {
+        const res = await fetch(
+          'https://api.themoviedb.org/3/movie/550?api_key=fa44637c4e3d45f7962fe730ff605501'
+        )
+        const movie = await res.json()
+        this.movie = movie
+      } catch (e) {
+        console.log(e)
+      }
+    },
+  },
+}
+</script>
+
+<style></style>
