89f6b805c6b0e2ff8fb527794674d5b30b851eaa
Deigan Millikin <dmillikin@fieldsafesolutions.com>
2021-02-15 18:03:34 -0700

    post - Animation In Vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,6 +3,11 @@
     <h1>
       <router-link to="/">{{ title }}</router-link>
     </h1>
+    <transition name="fade">
+      <h1 v-if="show">Animated</h1>
+    </transition>
+    <!-- Good for a basic toggle button -->
+    <button @click="show = !show">Show/Hide</button>
   </header>
 </template>
 
@@ -11,6 +16,11 @@ export default {
   name: 'Header',
   props: {
     title: String,
+  },
+  data() {
+    return {
+      show: false,
+    }
   },
 }
 </script>
@@ -24,5 +34,22 @@ header {
 
 h1 {
   margin: 0;
+}
+
+/* name-enter -> name-enter-to */
+/* name-enter-active */
+
+/* name-leave -> name-leave-to */
+/* name-leave-active */
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: all 0.3s ease;
+}
+
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+  transform: scale(0);
 }
 </style>
